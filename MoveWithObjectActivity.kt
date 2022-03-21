3a2ec7e4f7b09eb86be5b726ffedd731fbdad872
GopindoFazriM <101939157+GopindoFazriM@users.noreply.github.com>
2022-03-21 15:36:16 +0700

    Add files via upload

---
+++ MoveWithObjectActivity.kt
@@ -0,0 +1,23 @@
+package com.example.myintentappgopindo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MoveWithObjectActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_PERSON = "extra_person"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_with_object)
+
+        val tvObject: TextView =
+            findViewById(R.id.tv_object_received)
+
+        val person = intent.getParcelableExtra<Person>(EXTRA_PERSON) as Person
+        val text = " NAMA : ${person.NAMA.toString()},\n EMAIL : ${person.EMAIL},\n UMUR : ${person.UMUR},\n TEMPAT TINGGAL : ${person.KOTA}"
+        tvObject.text = text
+    }
+}
